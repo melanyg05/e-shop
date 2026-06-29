d9dc94349bd03b6a9cb8f25736459f0ba6c911c1
Melany Guañuna <mguanunas@est.ups.edu.ec>
2026-06-29 14:48:12 -0500

    Agregar public/home.html

--- app/config.py
+++ app/config.py
@@ -5,9 +5,9 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
-    #Configuracion de la DB
-    SQLALCHEMY_DATABASE_URI=(
+    # Configuración de la BD
+    SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
         f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     )
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
