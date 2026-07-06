882702286e435565ba9c8bac079d3e2549e149df
Melany Guañuna <mguanunas@est.ups.edu.ec>
2026-07-05 20:09:12 -0500

    Seed.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+basedir = Path(__file__).resolve().parent.parent
+load_dotenv(basedir / '.env')
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret')
